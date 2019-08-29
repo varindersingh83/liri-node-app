a3e442c69ed4eb5adc6022b05f6eb02dd651ad45
Varinder Singh <varinder83singh@gmail.com>
2019-08-28 23:12:54 -0700

    spotify

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 var keys = require('./keys.js')
+var Spotify = require('node-spotify-api')
 var spotify = new Spotify(keys.spotify)
 
 /*
@@ -8,3 +9,21 @@ var spotify = new Spotify(keys.spotify)
    - `movie-this`
    - `do-what-it-says`
 */
+
+spotify.search({ type: 'track', query: 'The Lion sleeps tonight' }, function(
+  err,
+  data
+) {
+  if (err) {
+    return console.log('Error occurred: ' + err)
+  }
+
+  console.log(
+    'Artist = ' + JSON.stringify(data.tracks.items[0].album.artists[0].name)
+  )
+  console.log('Song = ' + JSON.stringify(data.tracks.items[0].name))
+  console.log(
+    'Link = ' + JSON.stringify(data.tracks.items[0].external_urls.spotify)
+  )
+  console.log('Album = ' + JSON.stringify(data.tracks.items[0].album.name))
+})
